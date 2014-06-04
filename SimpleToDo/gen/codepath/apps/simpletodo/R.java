8778d5156eff69119f6a858833378f30663c24b9
gelita <rosas_angela@yahoo.com>
2014-06-04 15:02:15 -0700

    Updated UI - moving edit text to the top of screen so as to not conflict w/visibility while using keyboard. Added user info to explain basic functionality

--- SimpleToDo/gen/codepath/apps/simpletodo/R.java
+++ SimpleToDo/gen/codepath/apps/simpletodo/R.java
@@ -22,15 +22,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_dialog_alert=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btnAddItem=0x7f070006;
+        public static final int btnAddItem=0x7f070005;
         public static final int btnSaveEditItem=0x7f070003;
         public static final int container=0x7f070000;
         public static final int etEditItem=0x7f070002;
-        public static final int etNewItem=0x7f070005;
-        public static final int lvItems=0x7f070004;
+        public static final int etNewItem=0x7f070004;
+        public static final int lvItems=0x7f070006;
         public static final int tvEditItem_label=0x7f070001;
     }
     public static final class layout {
@@ -42,11 +43,17 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int btnAddItem=0x7f050003;
         public static final int btnSaveEditItem=0x7f050008;
+        public static final int cancel=0x7f05000a;
+        public static final int confirm_delete=0x7f05000c;
+        public static final int delete_confirm=0x7f05000b;
         public static final int etEditItem_hint=0x7f050007;
         public static final int etNewItem_hint=0x7f050004;
         public static final int hello_world=0x7f050001;
+        public static final int ok=0x7f05000e;
         public static final int title_activity_edit_item=0x7f050005;
+        public static final int title_activity_main=0x7f05000d;
         public static final int tvEditItem_label=0x7f050006;
+        public static final int userGuide=0x7f050009;
     }
     public static final class style {
         /** 
--- SimpleToDo/src/codepath/apps/simpletodo/TodoActivity.java
+++ SimpleToDo/src/codepath/apps/simpletodo/TodoActivity.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import org.apache.commons.io.FileUtils;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +25,9 @@ public class TodoActivity extends Activity {
 	ArrayList<String> items;
 	ArrayAdapter<String> itemsAdapter;
 	ListView lvItems;
+	int longToast = Toast.LENGTH_LONG;
+	int shortToast = Toast.LENGTH_SHORT;	
+		
 	private final int REQUEST_CODE = 20;
 
 	@Override
@@ -30,39 +36,64 @@ public class TodoActivity extends Activity {
 		setContentView(R.layout.activity_todo);
 		lvItems = (ListView) findViewById(R.id.lvItems);
 		items = new ArrayList<String>();
-		//loading items in onCreate
+		//loading items
 		readItems();
 		itemsAdapter = new ArrayAdapter<String>(this, 
 				android.R.layout.simple_list_item_1, items);
-		lvItems.setAdapter(itemsAdapter);
-		items.add("First Item");
-		items.add("Second Item"); 
-		setupListViewListener();
-	} 		   
-
-	private void readItems() {
-			File filesDir= getFilesDir();
-			File todoFile = new File(filesDir,"todo.text");
-			try{
-				items = new ArrayList<String>(FileUtils.readLines(todoFile));
-			}catch (IOException e){
-				items = new ArrayList<String>();
-				e.printStackTrace();
-			}
-	}			
-
-	private void setupListViewListener(){
-		lvItems.setOnItemLongClickListener(new OnItemLongClickListener(){
+		lvItems.setAdapter(itemsAdapter);		
+		setupListViewListener();		
+		showUserGuide();		
+	} 
+	private void showUserGuide(){
+		AlertDialog.Builder builder = new AlertDialog.Builder(TodoActivity.this);
+	    builder.setMessage("Click to edit entries.\nLong press to delete entries.")
+	    .setNeutralButton(R.string.ok, new DialogInterface.OnClickListener(){
 			@Override
-			public boolean onItemLongClick(AdapterView<?> parent,
-					View view, int position, long rowId){
-				items.remove(position);
-				itemsAdapter.notifyDataSetChanged();
-				saveItems();
-				return true;    		
+			public void onClick(DialogInterface dialog, int which) {
+				return;				
+			} 	
+	    })
+	    .setTitle("").setIcon(R.drawable.ic_launcher).create();
+	    builder.show();	    
+	}	
+	private void readItems() {
+		//returns abs path where files go for this app
+		File filesDir= getFilesDir();
+		File todoFile = new File(filesDir,"todo.text");
+		try{
+			items = new ArrayList<String>(FileUtils.readLines(todoFile));
+			//exception occurs if there is no file
+		}catch (IOException e){
+			items = new ArrayList<String>();
+		}
+	}	
+	private void setupListViewListener(){		
+		lvItems.setOnItemLongClickListener(new OnItemLongClickListener(){			
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent,View view, final int position, long rowId){
+				AlertDialog.Builder builder = new AlertDialog.Builder(TodoActivity.this);
+			    builder.setMessage("Are you sure you want to delete this entry?")
+			    .setPositiveButton(R.string.confirm_delete, new DialogInterface.OnClickListener() {
+			        public void onClick(DialogInterface dialog, int which) { 
+			            // continue with delete
+			        	items.remove(position);
+						itemsAdapter.notifyDataSetChanged();
+						saveItems();
+						return;
+			        }
+			     })
+			    .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+			        public void onClick(DialogInterface dialog, int which) { 
+			            // do nothing
+			        	return;
+			        }
+			     })
+			     .setTitle("Delete entry").setIcon(R.drawable.ic_dialog_alert).show();
+				 builder.create();
+				 return true;			
 			}				  
-		});
-
+		});		
+		
 		lvItems.setOnItemClickListener(new OnItemClickListener(){
 			@Override		
 			public void onItemClick(AdapterView<?> parent,
@@ -79,18 +110,17 @@ public class TodoActivity extends Activity {
 				startActivityForResult(i, REQUEST_CODE);
 			};			
 		});	
-	}
-	
+	}	
 	private void saveItems() {
 		File filesDir = getFilesDir();
 		File todoFile = new File(filesDir, "todo.txt");
 		try{
-			FileUtils.writeLines(todoFile, items);										
+			FileUtils.writeLines(todoFile, items);	
 		}catch (IOException e) {
 			e.printStackTrace();
-		}
+				return;
+		}		
 	}
-	
 	public void addTodoItem(View v){
 		EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
 		if(etNewItem.getText().toString().length() == 0 ){
@@ -102,16 +132,44 @@ public class TodoActivity extends Activity {
 			saveItems();//write to file
 			return;
 		}
-	}
-	
+	}	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		  if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {			  
 			  String dataItem =  data.getStringExtra("item");
 			  int position = data.getIntExtra("position", 0);			  
-		      Toast.makeText(this, dataItem, Toast.LENGTH_SHORT).show();
+		      makeToast(this, dataItem + " edited", shortToast);
 		      items.set(position, dataItem);
 			  itemsAdapter.notifyDataSetChanged();
 			  saveItems();	      
 		    }
-	}	
+	}
+	public static void makeToast (Context context, String toastString, int duration) {
+		Toast.makeText(context, toastString, duration).show();		
+		return;
+	}
+	
+	@Override
+	  protected void onPause() {
+	    super.onPause();
+	  }
+
+	  @Override
+	  protected void onResume() {
+	    super.onResume();
+	  }
+
+	  @Override
+	  protected void onStop() {
+	    super.onStop();
+	  }
+
+	  @Override
+	  protected void onDestroy() {
+	    super.onDestroy();
+	  }
+
+	  @Override
+	  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+	    super.onRestoreInstanceState(savedInstanceState);
+	  }
 }
